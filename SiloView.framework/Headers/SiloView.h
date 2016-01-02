566be8f946507306bcb97f95df70cbf8f83c68cd
URBOP <josequintero@24.0.10.in-addr.arpa>
2016-01-01 22:05:47 -0600

    Added Framework

---
+++ SiloView.framework/Headers/SiloView.h
@@ -0,0 +1,19 @@
+//
+//  SiloView.h
+//  SiloView
+//
+//  Created by Jose Quintero on 1/1/16.
+//  Copyright © 2016 Quintero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SiloView.
+FOUNDATION_EXPORT double SiloViewVersionNumber;
+
+//! Project version string for SiloView.
+FOUNDATION_EXPORT const unsigned char SiloViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SiloView/PublicHeader.h>
+
+
