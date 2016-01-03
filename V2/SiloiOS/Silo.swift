c76d8659fae0c6c9443d784e128082eb962d5d77
URBOP <josequintero@Joses-MacBook-Pro.local>
2016-01-03 13:07:39 -0600

    Minor Changes
    Merged Silo with SiloExt, New Zip

--- V2/SiloiOS/Silo.swift
+++ V2/SiloiOS/Silo.swift
@@ -9,7 +9,52 @@
 import Foundation
 import UIKit
 
-public class SiloView: UIView, NSURLSessionDelegate{
+extension UIImageView{
+    
+    func setLoaderColorForSiloView(color: UIColor) -> Bool{
+        for i in 0..<self.subviews.count{
+            if let sView = self.subviews[i] as? SiloView{
+                sView.loaderColor = color
+                return true
+            }
+        }
+        return false
+    }
+    
+    func toSiloView(withUrl: String){
+        for i in 0..<self.subviews.count{
+            if let sView = self.subviews[i] as? SiloView{
+                sView.removeFromSuperview()
+            }
+        }
+        self.clipsToBounds = true
+        let cM = self
+        let siloView = SiloView(frame: CGRectMake(0, 0, cM.bounds.width, cM.bounds.height))
+        siloView.backgroundColor = self.backgroundColor
+        siloView.opaque = false
+        siloView.fetch(withUrl, parent: cM)
+        self.addSubview(siloView)
+    }
+    
+    func toSiloView(withUrl: String, loaderColor: UIColor){
+        for i in 0..<self.subviews.count{
+            if let sView = self.subviews[i] as? SiloView{
+                sView.removeFromSuperview()
+            }
+        }
+        self.clipsToBounds = true
+        let cM = self
+        let siloView = SiloView(frame: CGRectMake(0, 0, cM.bounds.width, cM.bounds.height))
+        siloView.backgroundColor = self.backgroundColor
+        siloView.opaque = false
+        siloView.fetch(withUrl, parent: cM)
+        self.addSubview(siloView)
+        self.setLoaderColorForSiloView(loaderColor)
+    }
+    
+}
+
+internal class SiloView: UIView, NSURLSessionDelegate{
     //public
     var loaderColor: UIColor! = UIColor.darkGrayColor()
     ///////////////////
@@ -21,7 +66,7 @@ public class SiloView: UIView, NSURLSessionDelegate{
     private var parent: UIImageView!
     //////////
     
-    public func fetch(url: String, parent: UIImageView){
+    internal func fetch(url: String, parent: UIImageView){
         self.backgroundColor = parent.backgroundColor
         self.url = url
         self.parent = parent
@@ -31,19 +76,13 @@ public class SiloView: UIView, NSURLSessionDelegate{
     
     private var downloadTask: NSURLSessionDownloadTask?
     
-    override public func drawRect(rect: CGRect) {
+    override internal func drawRect(rect: CGRect) {
         let bounds = self.bounds
-        drawProgressLoader(frame: CGRectMake((bounds.width / 2) - 20, (bounds.height / 2) - 20, 40, 40), shapeColor: self.loaderColor, progressFraction: CGFloat(self.progress))
+        drawProgressLoader(frame: bounds, shapeColor: self.loaderColor, progressFraction: CGFloat(self.progress))
         if runned == false{
             createDownloadTask(url)
             runned = true
         }
-    }
-    
-    func fetchUrl(url:String){
-        self.url = url
-        runned = false
-        self.setNeedsDisplay()
     }
     
     func createDownloadTask(url: String) {
@@ -59,7 +98,7 @@ public class SiloView: UIView, NSURLSessionDelegate{
         if (totalBytesExpectedToWrite != -1){
             progressInternal = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
         }else {
-            progressInternal = self.progress + 0.2
+            progressInternal = self.progress + 0.1
             if progressInternal > 1{
                 progressInternal = 0
             }
@@ -69,13 +108,10 @@ public class SiloView: UIView, NSURLSessionDelegate{
     }
     
     func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didFinishDownloadingToURL location: NSURL) {
-        if self.progress == 1 {
-            let img = UIImage(data: NSData(contentsOfURL: location)!)
-            print(parent)
-            self.parent.image = img
-            
-            self.removeFromSuperview()
-        }
+        self.removeFromSuperview()
+        let img = UIImage(data: NSData(contentsOfURL: location)!)
+        print(parent)
+        self.parent.image = img
     }
     
     func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
@@ -86,16 +122,27 @@ public class SiloView: UIView, NSURLSessionDelegate{
 }
 
 extension SiloView{
-    func drawProgressLoader(frame frame: CGRect = CGRectMake(0, 0, 200, 200), shapeColor: UIColor = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1.000), progressFraction: CGFloat = 0) {
+    
+    func drawProgressLoader(frame frame: CGRect = CGRectMake(0, 0, 200, 200), shapeColor: UIColor = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1.000), progressFraction: CGFloat = 0.793) {
+        
+        //// Variable Declarations
         let expression: CGFloat = progressFraction != 0 ? progressFraction * -360 : 1
         
         
-        let ovalPath = UIBezierPath(ovalInRect: CGRectMake(frame.minX + floor(frame.width * 0.05500 + 0.5), frame.minY + floor(frame.height * 0.05000 + 0.5), floor(frame.width * 0.95000 + 0.5) - floor(frame.width * 0.05500 + 0.5), floor(frame.height * 0.94500 + 0.5) - floor(frame.height * 0.05000 + 0.5)))
+        //// Subframes
+        let group: CGRect = CGRectMake(frame.minX + floor((frame.width - 22) * 0.50000 + 0.5), frame.minY + floor((frame.height - 22) * 0.50000 + 0.5), 22, 22)
+        
+        
+        //// Group
+        //// Oval Drawing
+        let ovalPath = UIBezierPath(ovalInRect: CGRectMake(group.minX, group.minY, 22, 22))
         shapeColor.setStroke()
-        ovalPath.lineWidth = 5
+        ovalPath.lineWidth = 2.5
         ovalPath.stroke()
         
-        let oval2Rect = CGRectMake(frame.minX + floor(frame.width * 0.10500 + 0.5), frame.minY + floor(frame.height * 0.10000 + 0.5), floor(frame.width * 0.90000 + 0.5) - floor(frame.width * 0.10500 + 0.5), floor(frame.height * 0.89500 + 0.5) - floor(frame.height * 0.10000 + 0.5))
+        
+        //// Oval 2 Drawing
+        let oval2Rect = CGRectMake(group.minX + 1, group.minY + 1, 20, 20)
         let oval2Path = UIBezierPath()
         oval2Path.addArcWithCenter(CGPointMake(oval2Rect.midX, oval2Rect.midY), radius: oval2Rect.width / 2, startAngle: -1 * CGFloat(M_PI)/180, endAngle: -expression * CGFloat(M_PI)/180, clockwise: true)
         oval2Path.addLineToPoint(CGPointMake(oval2Rect.midX, oval2Rect.midY))
@@ -104,4 +151,5 @@ extension SiloView{
         shapeColor.setFill()
         oval2Path.fill()
     }
+    
 }
